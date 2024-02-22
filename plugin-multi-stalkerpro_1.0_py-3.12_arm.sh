330093f18a6522adaf4d08252711a35f29add055
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-02-22 23:25:01 +0200

    Update plugin-multi-stalkerpro_1.0_py-3.12_arm.sh

--- plugin-multi-stalkerpro_1.0_py-3.12_arm.sh
+++ plugin-multi-stalkerpro_1.0_py-3.12_arm.sh
@@ -1,33 +1,12 @@
+
 #!/bin/sh
 #
-cd /tmp
-set -e 
- wget "https://raw.githubusercontent.com/Ham-ahmed/multi-stalkerpro/main/multi-stalkerpro_1.0_py-3.12_arm.tar.gz"
+
+wget -O /var/volatile/tmp/enigma2-plugin-extensions-multi-stalkerpro_1.0_py-3.12_arm.ipk "https://raw.githubusercontent.com/Ham-ahmed/multi-stalkerpro/main/enigma2-plugin-extensions-multi-stalkerpro_1.0_py-3.12_arm.ipk"
 wait
-tar -xzf multi-stalkerpro_1.0_py-3.12_arm.tar.gz  -C /
+opkg install --force-overwrite /tmp/*.ipk
 wait
-cd ..
-set +e
-rm -f /tmp/multi-stalkerpro_1.0_py-3.12_arm.tar.gz
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-multi-stalkerpro_1.0_py-3.12_arm.ipk
+wait
 sleep 2;
-echo "" 
-echo "" 
-echo "**************************************************************************************************"
-echo "#  INSTALLED SUCCESSFULLY #"
-echo "*      888888  888888    .d88b.    88888     88    88   .d88b.  *"
-echo "*      88  88  88  88   88    88   88   88   88    88  88    88 *"
-echo "*      88    88    88  88      88  88    88  88oooo88  88oooo88 *"
-echo "*      88          88   88    88   88    88  88    88  88    88 *"
-echo "*      88          88   .d88b.     888888    88    88  88    88 *"
-echo "*          EDITE - Hamdy Ahmed       Novaler 4k Egypt *"
-echo "*                Enigma2 restart is required            *"
-echo "*************************************************************************************************"
-echo "   UPLOADED BY  >>>>   HAMDY_AHMED "
-sleep 4;
-	echo '========================================================================================='
-###########################################                                                                                                                  
-echo ". >>>>         RESTARING     <<<<"
-echo "*************************************************************************************************"
-wait
-killall -9 enigma2
 exit 0
